15540bf47a793f53559386cd7bd10630d098c65e
Santiago Rocha <santiago.infsec@gmail.com>
2019-11-18 15:01:05 +0100

    Added some functionality and handled errors

--- MAChanger.py
+++ MAChanger.py
@@ -2,26 +2,48 @@
 
 import subprocess
 import argparse
+import re
 
-parser = argparse.ArgumentParser()
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address")
+    args = parser.parse_args()
+    if not args.interface:
+        parser.error("[-] Specify interface, use --help for more info")
+    elif not args.new_mac:
+        parser.error("[-] Specify MAC address, use --help for more info")
+    return args
 
-parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address")
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+    subprocess.call(["/sbin/ifconfig", interface, "down"])
+    subprocess.call(["/sbin/ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["/sbin/ifconfig", interface, "up"])
+    op = raw_input("Do you want to show the config for " + interface + " ? [y/n] > ")
+    if op == 'y' or op == 'Y':    
+        subprocess.call(["/sbin/ifconfig", interface])
+    elif not (op != 'y' or op != 'Y') and (op != 'n' or op != 'N'):
+        print("You must enter y [yes] or n [no]")
 
-args = parser.parse_args()
+def get_mac(interface):
+    ifconfig_output = subprocess.check_output(["/sbin/ifconfig", interface])
+    search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_output)
 
-interface = args.interface
-new_mac = args.new_mac
+    if search_result:
+        return search_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
 
-print("[+] Changing MAC Address for " + interface + " to " + new_mac)
+args = get_arguments()
+current_mac = get_mac (args.interface)
+print("Current MAC = " + str(current_mac))
+change_mac(args.interface, args.new_mac)
 
-subprocess.call(["/sbin/ifconfig", interface, "down"])
-subprocess.call(["/sbin/ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["/sbin/ifconfig", interface, "up"])
-op = raw_input("Do you want to show the new config for " + interface + " ? [y/n] > ")
-if op == 'y' or op == 'Y':    
-    subprocess.call(["/sbin/ifconfig", interface])
-elif op == 'n' or op == 'N':
-    print("[+] Done")
+current_mac = get_mac (args.interface)
+if current_mac == args.new_mac:
+    print("[+] Succesfully changed MAC to " + current_mac)
 else:
-    print("Error")
+    print("[-] Error when changing MAC")
+
+
